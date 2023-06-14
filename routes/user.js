82931c94a5284b4a2851ef264c8e51b88e25529e
Vishal Sharma <vishals200415@gmail.com>
2023-06-14 23:24:47 +0530

    User Route set up

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const { verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 const router = require("express").Router();
 
 // UPDATE
@@ -22,6 +22,72 @@ router.put("/:id", verifyTokenAndAuthorization, async (req,res) => {
         // Send our updated user.
         res.status(200).json(updatedUser);
     }catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+// DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req,res) =>{
+    try{
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User Deleted...");
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+// GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async (req,res) =>{
+    try{
+        const user = await User.findById(req.params.id);
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+// GET ALL USERS
+router.get("/", verifyTokenAndAdmin, async (req,res) =>{
+    // This will check for any queries in our request. Queries -> url?query=any. Here "new" will be searched in queries.
+    const query = req.query.new;
+    try{
+        // this will check for query. If query is found then it will sort the users from new to old using "sort({_id:-1})" and "limit()" sets how many users will be returned.
+        const users = query
+        ?await User.find().sort({_id:-1}).limit(1)
+        :await User.find();
+        res.status(200).json(users);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+// GET USER STATS
+router.get("/stats", verifyTokenAndAdmin, async (req,res) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear()-1));
+
+    try{
+        //get all the createdAt time stamps of the users. $gte will do the comparison of greater than.
+        const data = await User.aggregate([
+            {$match: {createdAt: {$gte: lastYear}}},
+            {
+                // get the month number from created at timestamp and assign it to month variable.
+                $project: {
+                    month: {$month: "$createdAt"},
+                }
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: {$sum:1},
+                }
+            }
+        ]);
+
+        res.status(200).json(data);
+
+    }catch(err){
         res.status(500).json(err);
     }
 });
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -6,6 +6,7 @@ const verifyToken = (req,res,next) =>{
         const token = authHeader;
         jwt.verify(token, process.env.JWT_SEC, (err,user) => {
             if(err) res.status(403).json("Token invalid!");
+            // user here stores the user id and whether the user is admin or not.
             req.user = user;
             next();
         });
@@ -16,6 +17,7 @@ const verifyToken = (req,res,next) =>{
 
 const verifyTokenAndAuthorization = (req,res,next) => {
     verifyToken(req,res,()=>{
+        // params.id will fetch id from our request i.e. from the url.
         if(req.user.id === req.params.id || req.user.isAdmin){
             next();
         }else{
@@ -24,4 +26,14 @@ const verifyTokenAndAuthorization = (req,res,next) => {
     });
 };
 
-module.exports = {verifyToken, verifyTokenAndAuthorization};
+const verifyTokenAndAdmin = (req,res,next) => {
+    verifyToken(req,res,()=>{
+        if(req.user.isAdmin){
+            next();
+        }else{
+            res.status(403).json("Invalid Access!!");
+        }
+    });
+};
+
+module.exports = {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin};
